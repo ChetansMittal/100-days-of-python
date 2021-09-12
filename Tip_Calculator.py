991227f528670a51d93b179c9b069770c653e0f7
ChetansMittal <76930533+ChetansMittal@users.noreply.github.com>
2021-09-12 21:18:52 +0530

    Delete Tip_Calculator.py

--- Tip_Calculator.py
+++
@@ -1,9 +0,0 @@
-print("Welcome to the tip calculator.")
-bill = input("What was the total bill?")
-total_bill =float(bill)
-tip =int( input("What percentage tip would you like to give? 10, 12, or 15?"))
-final_tip=float((tip/100*total_bill)+total_bill)
-people= int(input("How many people to split the bill?"))
-pay = final_tip/people
-final_pay = round(pay,2)
-print(f"Each person should pay: ${final_pay}")
